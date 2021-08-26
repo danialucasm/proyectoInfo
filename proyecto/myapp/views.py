9580ccf68697d50b63f0c4f1b2fbdcc70aaf58e8
JUDIIB <juanma_gsm@hotmail.com>
2021-08-26 20:07:29 -0300

    guardando

--- proyecto/myapp/views.py
+++ proyecto/myapp/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 # Create your views here.
 
 def register(request):
+    form = UserRegisterForm()
     if request.method =='POST': #Para utilizar los campos que fueron llenados en el form, else para q se muetre form vacio
         form = UserRegisterForm(request.POST) #para acceder a la info
         if form.is_valid():
@@ -14,10 +15,9 @@ def register(request):
             username = form.cleaned_data['username'] 
             messages.success(request, f'Usuario {username} creado')
             return redirect('login') #como ya se registro ahora se tiene que ir a loguear
-    else: 
-        form = UserRegisterForm()
-        context = {'form' : form}
-        return render(request, 'myapp/register.html', context) #para que se mantenga en register
+
+    context = {'form' : form}
+    return render(request, 'myapp/register.html', context) #para que se mantenga en register
 
 def perfil(request):
     return render(request, 'myapp/perfil.html')
