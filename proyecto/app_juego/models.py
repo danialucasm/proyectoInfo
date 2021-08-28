5fb396db7b93013be145fe08cdce1a03fa9fe1b6
danialucasm <danulucas44@gmail.com>
2021-08-28 04:02:28 -0300

    Avances front + juego

--- proyecto/app_juego/models.py
+++ proyecto/app_juego/models.py
@@ -20,7 +20,7 @@ class ElegirRespuesta(models.Model):
 
     MAXIMO_RESPUESTA = 3
 
-    pregunta = models.ForeignKey(Pregunta, related_name='preguntas', on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta, related_name='opciones', on_delete=models.CASCADE)
     correcta = models.BooleanField(verbose_name='¿Es esta la pregunta correcta?', default=False, null=False)
     texto = models.TextField(verbose_name='Texto de la respuesta')
 
@@ -33,10 +33,41 @@ class TriviaUsuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     puntaje_total = models.DecimalField(verbose_name='Puntaje Total', default=0, decimal_places=2, max_digits=10)
 
+    def crear_intentos(self, pregunta):
+        intento = PreguntasRespondidas(pregunta=pregunta, triviaUser=self)
+        intento.save()
+
+    def obtener_nuevas_preguntas(self):
+        respondidas = PreguntasRespondidas.objects.filter(triviaUser=self).values_list('pregunta__pk', flat=True)
+        preguntas_restantes = Pregunta.objects.exclude(pk__in=respondidas)
+        if not preguntas_restantes.exists():
+            return None
+        return random.choice(preguntas_restantes) #Para que toque cualquier pregunta
+    
+    def validar_intento(Self, pregunta_respondida, respuesta_seleccionada):
+        if pregunta_respondida.pregunta_id != respuesta_seleccionada.pregunta_id:
+            return
+        
+        pregunta_respondida.respuesta_seleccionada = respuesta_seleccionada
+        if respuesta_seleccionada.correcta is True:
+            pregunta_respondida.correcta = True
+            pregunta_respondida.puntaje_obtenido = respuesta_seleccionada.pregunta.max_puntaje
+            pregunta_respondida.respuesta = respuesta_seleccionada
+
+        else:
+            pregunta_respondida.respuesta = respuesta_seleccionada
+
+
+        pregunta_respondida.save()
+        
+        Self.actualizar_puntaje()
+
+
+
 class PreguntasRespondidas(models.Model):
-    triviaUser = models.ForeignKey(TriviaUsuario, on_delete=models.CASCADE)
+    triviaUser = models.ForeignKey(TriviaUsuario, on_delete=models.CASCADE, related_name='intentos')
     pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
-    respuesta = models.ForeignKey(ElegirRespuesta, on_delete=models.CASCADE, related_name='intentos')
+    respuesta = models.ForeignKey(ElegirRespuesta, on_delete=models.CASCADE, null=True)
     correcta = models.BooleanField(verbose_name='¿Es esta la respuesta correcta?', default=False, null=False)
     puntaje_obtenido = models.DecimalField(verbose_name='Puntaje Obtenido', default=0, decimal_places=2, max_digits=6)
 
--- proyecto/app_juego/views.py
+++ proyecto/app_juego/views.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
 from .models import TriviaUsuario, Pregunta, PreguntasRespondidas
 from django.contrib.admin.sites import AdminSite
 from django.shortcuts import redirect, render
+from django.http.response import Http404
 # Create your views here.
 
 
@@ -8,15 +10,25 @@ def inicio(request):
     context = {}
     return render(request, 'app_juego/inicio.html', context)
 
+
 def jugar(request):
     TriviaUser, created = TriviaUsuario.objects.get_or_create(usuario=request.user)
     if request.method == 'POST':
         pregunta_pk = request.POST.get('pregunta_pk')
         pregunta_respondida = TriviaUsuario.intentos.select_releated('pregunta').get(pregunta_pk=pregunta_pk)
         respuesta_pk = request.POST.get('respuesta_pk')
+
+        try:
+            opcion_seleccionada = pregunta_respondida.pregunta.opciones.get(pk=respuesta_pk)
+        except ObjectDoesNotExist:
+            raise Http404
+        TriviaUser.validar_intento(pregunta_respondida,opcion_seleccionada)
+        return redirect(pregunta_respondida)
     else:
-        respondidas = PreguntasRespondidas.objects.filter(triviaUser=TriviaUser).values_list('pregunta__pk', flat=True)
-        pregunta = Pregunta.objects.exclude(pk__in=respondidas)
+        pregunta = TriviaUser.obtener_nuevas_preguntas()        
+        if pregunta is not None:
+            TriviaUser.crear_intentos(pregunta)
+
         context= {
             'pregunta':pregunta
         }
