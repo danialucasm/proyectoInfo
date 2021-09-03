26a8d334bed5b822a5c7ed1c0a0bc0a08b5397e6
danialucasm <danulucas44@gmail.com>
2021-09-02 21:07:09 -0300

    Use_tx en false

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -99,15 +99,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-AR'
+LANGUAGE_CODE ='es-AR'
 
-TIME_ZONE = 'America/Argentina/Buenos_Aires'
+TIME_ZONE ='America/Argentina/Buenos_Aires'
 
-USE_I18N = True
+USE_I18N =True
 
-USE_L10N = True
+USE_L10N =True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
